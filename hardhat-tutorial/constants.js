b3ae27413df98eb9af9020a7d5f5cdfc104d480d
PLMSCE <andrewpeters.0x@gmail.com>
2022-12-05 12:32:12 -0500

    Initial commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x873380F790fa66580B95209f2f14Ad08726325c5";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
